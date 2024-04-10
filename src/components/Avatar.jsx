170def91d7c938a09d6a7ca36c4a8e2501bdfd42
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2024-04-10 12:08:51 -0300

    Ajustes movimento 3D

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -15,33 +15,47 @@ export function Avatar({ sceneType, bounds = { minX: -Infinity, maxX: Infinity, 
 
     let speed = 0.05;
     let forward = new THREE.Vector3();
-    let side = new THREE.Vector3();
-
-
+    let backward = new THREE.Vector3();
+    let left = new THREE.Vector3();
+    let right = new THREE.Vector3();
+    let direction = new THREE.Vector3();
+  
     // Define os vetores de movimento com base no tipo de cena
     switch(sceneType) {
         case 'office':
-            forward.set(0, 1, -1);
-            side.set(1, 0, 0);
+            forward.set(0, 0, -1);
+            backward.set(0, 0, 1);
+            right.set(1, 0, 0);
+            left.set(-1, 0, 0);
             speed = 0.04;
             break;
         case 'arena':
-            forward.set(0, 1, 0);
-            side.set(1, 0, 0);
+            forward.set(0, 0, -1);
+            backward.set(0, 0, 1);
+            left.set(-1, 0, 0);
+            right.set(1, 0, 0);
             speed = 0.1;
             break;
         // Adicione mais casos conforme necessário
     }
 
-    forward.applyQuaternion(avatarRef.current.quaternion);
-    side.applyQuaternion(avatarRef.current.quaternion);
+    // Calcula a direção de movimento combinada
+if (movement.forward) direction.add(forward);
+if (movement.backward) direction.sub(forward); // Usar sub para mover na direção oposta
+if (movement.left) direction.sub(right); // Usar sub para esquerda, considerando 'right' como direção base
+if (movement.right) direction.add(right);
 
-    if (movement.forward) avatarRef.current.position.addScaledVector(forward, speed);
-    if (movement.backward) avatarRef.current.position.addScaledVector(forward, -speed);
-    if (movement.left) avatarRef.current.position.addScaledVector(side, -speed);
-    if (movement.right) avatarRef.current.position.addScaledVector(side, speed);
+// Normaliza a direção para garantir consistência na velocidade
+direction.normalize().multiplyScalar(speed);
 
-    
+// Atualiza a posição do avatar
+avatarRef.current.position.add(direction);
+
+ // Se houver movimento, ajuste a rotação do avatar
+ if (direction.lengthSq() > 0) {
+  const angle = Math.atan2(direction.x, direction.z);
+  avatarRef.current.rotation.z = angle;
+}
 
     // Aplica os limites específicos da cena
     avatarRef.current.position.clamp(new THREE.Vector3(bounds.minX, bounds.minY, bounds.minZ), new THREE.Vector3(bounds.maxX, bounds.maxY, bounds.maxZ));
@@ -54,8 +68,7 @@ export function Avatar({ sceneType, bounds = { minX: -Infinity, maxX: Infinity, 
       geometry={nodes.Object_2.geometry}
       material={materials.palette}
       position={[1, -0.2, -2]}
-      rotation={[-Math.PI / 2, 0, 0]}
-      
+      rotation={[-Math.PI / 2, 0, 0]}  
     />
   );
 }
